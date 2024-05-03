9c3995a6735d689530e05540742b69e27681381e
Kubaryt <kubastarzyk6554@gmail.com>
2024-05-03 22:11:52 +0200

    feat: Created core app

--- H3templatesWebsiteDjango/settings.py
+++ H3templatesWebsiteDjango/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = "django-insecure-f(%lwnj_&(%xer1v^k72p_5s1dm+y^yu79&s@a$i3=in+-gdlj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "core",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
